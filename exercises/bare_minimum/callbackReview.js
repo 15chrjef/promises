cf9fa9386db88f6c53bb0918e822cf7e754e366b
15chrjef <15chrjef@gmail.com>
2016-10-05 17:37:00 -0700

    commit

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -1,17 +1,57 @@
 /**
  * Implement these functions following the node style callback pattern
  */
-
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 var fs = require('fs');
 var request = require('request');
-
+var xhr = new XMLHttpRequest();
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, cb) {
   // TODO
+  return fs.readFile(filePath, 'utf8', function(err, results) {
+    if (err) {
+      cb(err);
+      return;
+    }
+    var firstLine = results.split('\n')[0];
+    cb(err, firstLine);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
+  
+  console.log('UNSENT', xhr.status);
+
+  xhr.open('GET', url, true);
+  console.log('OPENED', xhr.status);
+  // xhr.onreadystatechange = function(event) {
+  //   // console.log(xhr.responseText, xhr.response);
+  //   if (xhr.status === 200) {
+  //     callback(xhr.statusText, xhr.responseText);
+  //   } else {
+  //     callback(xhr.statusText);
+  //   }
+  // };
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      console.log('hello 200', xhr.status);
+      callback(undefined, xhr.status);
+      return;
+    } 
+  };
+  xhr.onerror = function() {
+    var err = {};
+    err.message = 'Invalid URI';
+    callback(err);
+  };
+  // if (xhr.status !== 200) {
+  //   console.log('failed',xhr.status)
+  //   callback(err);
+  // }
+  xhr.setRequestHeader('Content-type', 'text/html');
+  xhr.send();
+
   // TODO
 };
 
@@ -20,3 +60,11 @@ module.exports = {
   getStatusCode: getStatusCode,
   pluckFirstLineFromFile: pluckFirstLineFromFile
 };
+// fs.readFile(__dirname + '/README.md', 'utf8'
+//   console.log('Example from callbackReview.js')
+//   if (err) {
+//     console.log('fs.readFile failed :(\n', err)
+//   } else {
+//     console.log('fs.readFile successfully completed :)\n', content)
+//   }
+// });
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,6 +11,19 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise( (resolve, reject) => {
+    fs.readFile(filePath, 'utf8', function(err, results) {
+      if (err) {
+        reject(err);
+      }
+      resolve(results);
+    });
+  }).then(function(res) { 
+    return res.split('\n')[0];
+  }).catch(function(err) {
+    console.log(err);
+    return err;
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
